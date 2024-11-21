3af1d799cec3c77d7da5cec87218b9dd633ec539
Juan Sebastian Varon Peña <tatan.varonp@Juans-MacBook-Air.local>
2024-11-21 17:57:35 -0500

    JWT actializacion login usuario

--- middleware/jwt.js
+++ middleware/jwt.js
@@ -1,21 +1,23 @@
-const jwt = require ("jsonwebtoken")
-
-exports.verificar = async (req, res, next) => {
+exports.verificar = async (req, res, next)=> {
     try {
-        let token = req.headers.authorization
-        token = token.slplit (' ') [1]
-        let SECRET_KEY_JWT = process.env.SECRET_KEY_JWT
-        jwt.verify ( token, SECRET_KEY_JWT, (error, decoded) => {
+       let token = req.headers.authorization
+
+       if (token != undefined) { 
+           token = token.split(' ')[1]
+            let SECRET_KEY_JWT = process.env.SECRET_KEY_JWT
+           jwt.verify( token, SECRET_KEY_JWT, (error, decoded)=> {
 
             if (error) {
-                res.send ({error: "Ha ocurrido algo comunicate con el admin"})
+                res.status(400).send({error: "Token proporcionado invalido"})
             }
-
-        })
-       
-       
+             req.usuario = decoded
+            next()
+           })
+       } else {
+        res.status(400).send({error: "Token no proporcionado"})
+       }
     } catch (error) {
         console.log(error);
-        res.send ({error: "Ha ocurrido algo comunicate con el admin"})
+        res.status(500).send({error: "Ha ocurrido algo comunicate con el admin"})
     }
 }
